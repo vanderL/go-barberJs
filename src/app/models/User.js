8aa54ae75165c04df534c47e267602b87f489996
vanderL <vnd.vander@gmail.com>
2021-03-26 16:22:01 -0300

    JWT authentication

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -24,6 +24,11 @@ class User extends Model {
 
     return this;
   }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+
 }
 
 export default User;
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: '8f02c461bac2398fa15bbc40bb72fd27',
+    expiresIn: '7d',
+};
