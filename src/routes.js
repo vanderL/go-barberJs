a5b7becc0f208fbe706fafcbf512b478fbe7ff43
vanderL <vnd.vander@gmail.com>
2021-03-27 00:16:44 -0300

    Multer Config

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import multer from 'multer';
+import MulterConfig from './config/multer';
 
 import UserController from './app/controller/UserController'
 import SessionController from './app/controller/SessionController'
@@ -6,6 +8,7 @@ import SessionController from './app/controller/SessionController'
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(MulterConfig)
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -13,5 +16,9 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+    return res.json({ok: true});
+})
 
 export default routes;
