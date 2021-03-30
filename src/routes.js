97fdef81dda8aa3239017e7ccf061217f56f70c0
vanderL <vnd.vander@gmail.com>
2021-03-30 12:18:16 -0300

    Showing Providers

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import MulterConfig from './config/multer';
 import UserController from './app/controller/UserController'
 import SessionController from './app/controller/SessionController'
 import FileController from './app/controller/FileController'
+import ProviderController from './app/controller/ProviderController'
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -17,6 +18,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index)
 
 routes.post('/files', upload.single('file'), FileController.store)
 
