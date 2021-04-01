b0a52a7d2cfcc1402d9b97d1dc06823ef7bec96e
vanderL <vnd.vander@gmail.com>
2021-04-01 19:00:02 -0300

    SendMail with nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: '2525',
+    secure: false,
+    auth: {
+        user: "47c623bbdf355d",
+        pass: "42af2735f1ef0a"
+    },
+    default: {
+        from: 'Equipe GoBarber! <noreply@gobarber.com',
+    },
+};
