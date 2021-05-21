7823689c0fb5fa350ad0aae9b02bdb552dd7f5c9
vanderL <vnd.vander@gmail.com>
2021-05-20 21:33:57 -0300

    Storing profile and correction type of input on form singIn for password type

--- frontend/src/pages/singIn/index.js
+++ frontend/src/pages/singIn/index.js
@@ -30,7 +30,7 @@ export default function SingIn() {
       
       <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Seu e-mail" />
-        <Input name="password" ype="password" placeholder="Sua senha senha" />
+        <Input name="password" type="password" placeholder="Sua senha senha" />
 
         <button type="submit"> Acessar </button>
         <Link to="/register" > Criar conta gratuita </Link>
