78d3bca02409b1ccfa8176efff3daad3c2e6adce
vanderL <vnd.vander@gmail.com>
2021-05-29 22:18:12 -0300

    Page: Profile

--- frontend/src/pages/profile/index.js
+++ frontend/src/pages/profile/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
-function profile() {
-  return <h1>Oi sou o Profile</h1>;
+export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    console.log(data);
+  }
+  
+  return(
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome do completo"/>
+        <Input name="email" type="email" placeholder="Seu melhor e-mail"/>
+
+        <hr />
+
+        <Input type="password" name="oldPassword" placeholder="Sua senha atual"/>
+        <Input type="password" name="password" placeholder="Sua senha nova"/>
+        <Input type="password" name="confirmPassword" placeholder="Confirme sua senha"/>
+      
+        <button type="submit">Atualizar perfil</button>
+      
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  )
 }
-
-export default profile;
