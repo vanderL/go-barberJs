694c2addfd7eab0d4c729f0c0eb3cf3babab948f
vanderL <vnd.vander@gmail.com>
2021-05-31 16:26:47 -0300

    Styling Dashboard

--- frontend/src/pages/dashboard/index.js
+++ frontend/src/pages/dashboard/index.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+import { Container, Time } from './styles';
 
 
 export default function Dashboard() {
-  api.get('appointments');
-  
-  return <h1> Oi sou o Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff"/>
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Vander Lima</span>
+        </Time>
+        
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        
+        <Time>
+          <strong>09:00</strong>
+          <span>Vander Lima</span>
+        </Time>
+        
+        <Time>
+          <strong>09:00</strong>
+          <span>Vander Lima</span>
+        </Time>
+      </ul>
+    </Container>
+  )
 }
